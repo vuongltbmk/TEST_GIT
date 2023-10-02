031ed880c2444d84195d64dcb25722d2350c2ede
vuongltbmk <vuonglt.bmk@gmail.com>
2023-10-02 11:53:25 +0700

    6

--- TEST_GIT/Controllers/WeatherForecastController.cs
+++ TEST_GIT/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace TEST_GIT.Controllers
             int i3 = 0;
             int i4 = 0;
             int i5 = 0;
+            int ii6 = 0;
             string sql = "Excec store 12";
             string sq2l = "Excec store 12";
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
